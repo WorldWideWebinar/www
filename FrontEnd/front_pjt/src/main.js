fa53292fa3b1810db57a4db15b5e5daccf5b4987
박준영 <raypark1241@gmail.com>
2024-07-15 16:08:46 +0900

    Fix: bootstrap 미적용 이슈 해결중

--- FrontEnd/front_pjt/src/main.js
+++ FrontEnd/front_pjt/src/main.js
@@ -1,11 +1,10 @@
-
-import 'flatpickr/dist/flatpickr.css';
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniapluginPersistedstate from 'pinia-plugin-persistedstate'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import '@/assets/styles/main.css'
+import 'flatpickr/dist/flatpickr.css';
 import App from './App.vue'
 import router from './router'
 
