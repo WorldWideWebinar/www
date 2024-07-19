1d7830f7673023870df56f3d8bfacac730ba4f02
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-07-19 17:19:39 +0900

    Feat: MeetingSTT Stomp 구축
    - StompConfig 소켓 번호 5000으로 변경
    - StompConfig Endpoint 추가 ("/stomp/stt")

--- BackEnd/globalcc/src/main/java/com/ssafy/globalcc/config/socket/StompConfig.java
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/config/socket/StompConfig.java
@@ -9,15 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker //WebSocket 서버를 활성화
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
-
-    //endpoint를 /stomp로 하고, allowedOrigins를 "*"로 하면 페이지에서
-    //Get /info 404 Error가 발생한다. 그래서 아래와 같이 2개의 계층으로 분리하고
-    //origins를 개발 도메인으로 변경하니 잘 동작하였다.
-    //이유는 왜 그런지 아직 찾지 못함
+    
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("http://localhost:5000") // "*" 오류 발생
+                .withSockJS();
+        registry.addEndpoint("/stomp/stt")
+                .setAllowedOrigins("http://localhost:5000")
                 .withSockJS();
     }
 
