2b52a68a58540dd8e157c960dd9f148cd80e774c
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-07-30 09:53:33 +0900

    Feat: MeetingSTT 데이터 DB 저장

--- BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/meetingSTT/controller/MeetingSTTStompController.java
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/meetingSTT/controller/MeetingSTTStompController.java
@@ -1,6 +1,10 @@
 package com.ssafy.globalcc.domain.meetingSTT.controller;
 
+import com.ssafy.globalcc.domain.meeting.entity.Meeting;
+import com.ssafy.globalcc.domain.meeting.repository.MeetingRepository;
 import com.ssafy.globalcc.domain.meetingSTT.dto.request.MeetingSTTRequest;
+import com.ssafy.globalcc.domain.meetingSTT.entity.MeetingSTT;
+import com.ssafy.globalcc.domain.meetingSTT.repository.MeetingSTTRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +22,20 @@ public class MeetingSTTStompController {
 
     private static final String EXCHANGE_NAME = "meetingSTT.exchange";
     private static final String ROUTING_KEY = "meetingSTT.key";
+    private final MeetingSTTRepository meetingSTTRepository;
+    private final MeetingRepository meetingRepository;
 
     @MessageMapping("meetingSTT.{meetingId}")
     public void share(
             @DestinationVariable("meetingId") final Integer meetingId,
             @Payload final MeetingSTTRequest request
     ) {
+        Meeting meeting = meetingRepository.findById(Integer.valueOf(request.getMeetingId()))
+                .orElseThrow(() -> new IllegalArgumentException("Meeting not found"));
+        MeetingSTT meetingSTT = new MeetingSTT();
+        meetingSTT.setMeeting(meeting);
+        meetingSTT.setContent(request.getContent());
+        meetingSTTRepository.save(meetingSTT);
         rabbitTemplate.convertAndSend(
                 EXCHANGE_NAME, ROUTING_KEY + meetingId, request
         );
