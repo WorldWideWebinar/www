8dc300a167a1f43a3a4eab7f915cccd2a8793ba5
김수빈 <86918962+ksb3458@users.noreply.github.com>
2024-07-30 09:43:10 +0900

    Feat: RabbitMQ 메세징 구현

---
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/config/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,79 @@
+package com.ssafy.globalcc.config.rabbitmq;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
+public class RabbitMQConfig {
+
+    private static final String QUEUE_NAME = "meetingSTT.queue";
+    private static final String EXCHANGE_NAME = "meetingSTT.exchange";
+    private static final String ROUTING_KEY = "meetingSTT.key";
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost("localhost");
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        return factory;
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory, Queue queue, DirectExchange exchange, Binding binding) {
+        final RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.declareQueue(queue);
+        rabbitAdmin.declareExchange(exchange);
+        rabbitAdmin.declareBinding(binding);
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
--- BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/meetingSTT/controller/MeetingSTTStompController.java
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/meetingSTT/controller/MeetingSTTStompController.java
@@ -1,35 +1,32 @@
 package com.ssafy.globalcc.domain.meetingSTT.controller;
 
-import com.ssafy.globalcc.domain.meeting.entity.Meeting;
-import com.ssafy.globalcc.domain.meeting.repository.MeetingRepository;
 import com.ssafy.globalcc.domain.meetingSTT.dto.request.MeetingSTTRequest;
-import com.ssafy.globalcc.domain.meetingSTT.dto.response.MeetingSTTResponse;
-import com.ssafy.globalcc.domain.meetingSTT.entity.MeetingSTT;
-import com.ssafy.globalcc.domain.meetingSTT.repository.MeetingSTTRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
 public class MeetingSTTStompController {
+    private final RabbitTemplate rabbitTemplate;
 
-    private final SimpMessagingTemplate template;
-    private final MeetingSTTRepository meetingSTTRepository;
-    private final MeetingRepository meetingRepository;
+    private static final String EXCHANGE_NAME = "meetingSTT.exchange";
+    private static final String ROUTING_KEY = "meetingSTT.key";
 
-    @MessageMapping(value = "/stt/message")
-    public void message(MeetingSTTRequest request) {
-        Meeting meeting = meetingRepository.findById(Integer.valueOf(request.getMeetingId()))
-                .orElseThrow(() -> new IllegalArgumentException("Meeting not found"));
-        MeetingSTT meetingSTT = new MeetingSTT();
-        meetingSTT.setMeeting(meeting);
-        meetingSTT.setContent(request.getContent());
-
-        meetingSTTRepository.save(meetingSTT);
-        MeetingSTTResponse response = MeetingSTTResponse.of(request.getMeetingId(), request.getContent());
-        template.convertAndSend("/sub/stt/room/" + request.getMeetingId(), response);
+    @MessageMapping("meetingSTT.{meetingId}")
+    public void share(
+            @DestinationVariable("meetingId") final Integer meetingId,
+            @Payload final MeetingSTTRequest request
+    ) {
+        rabbitTemplate.convertAndSend(
+                EXCHANGE_NAME, ROUTING_KEY + meetingId, request
+        );
     }
-
 }
+
