8d62ea42211b06e1192c54e836c9bd657d3b423b
박준영 <raypark1241@gmail.com>
2024-07-31 14:21:13 +0900

    Fix: Islogin 판단기준

--- FrontEnd/front_pjt/src/stores/userStore.js
+++ FrontEnd/front_pjt/src/stores/userStore.js
@@ -1,96 +1,94 @@
-import { defineStore } from 'pinia';
-import axiosInstance from '@/axios';
-import { useTeamStore } from './teamStore';
-import router from '@/router';
-import { useErrorStore } from './errorStore';
+import { defineStore } from 'pinia'
+import axiosInstance from '@/axios'
+import { useTeamStore } from './teamStore'
+import router from '@/router'
+import { useErrorStore } from './errorStore'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     userId: 0,
     userInfo: {},
-    accessToken: null, 
+    accessToken: null,
     refreshToken: null,
-    userList: [],
+    userList: []
   }),
   getters: {
-    isLogin: (state) => !!state.accessToken,
+    isLogin: (state) => state.userId != 0
   },
-  
+
   actions: {
     async fetchUserInfo(userId) {
-      const errorStore = useErrorStore();
+      const errorStore = useErrorStore()
       try {
-        const response = await axiosInstance.get(`api/users/${userId}`);
-        const userData = response.data;
-        this.userInfo = userData;
+        const response = await axiosInstance.get(`api/users/${userId}`)
+        const userData = response.data
+        this.userInfo = userData
         console.log(response.data)
-        return userData;
+        return userData
       } catch (error) {
-        errorStore.showError('Failed to fetch user info');
+        errorStore.showError('Failed to fetch user info')
       }
     },
 
-    async fetchAllUsers(){
-      const errorStore = useErrorStore();
+    async fetchAllUsers() {
+      const errorStore = useErrorStore()
       try {
-        const response = await axiosInstance.get(`api/users`);
+        const response = await axiosInstance.get(`api/users`)
         const users = response.data.result
-        this.userList = users.map(user => ({
+        this.userList = users.map((user) => ({
           userId: user.userId,
-          id: user.id,
-        }));
-        console.log('1');
+          id: user.id
+        }))
+        console.log('1')
         console.log(this.userList)
-        console.log('1');
-        return this.userList;
+        console.log('1')
+        return this.userList
       } catch (error) {
-        errorStore.showError('Failed to fetch all users');
+        errorStore.showError('Failed to fetch all users')
       }
     },
 
     async signIn({ id, password }) {
-      const errorStore = useErrorStore();
+      const errorStore = useErrorStore()
       try {
-        const response = await axiosInstance.post('api/users/login', { id, password });
-        console.log(response.data);
+        const response = await axiosInstance.post('api/users/login', { id, password })
+        console.log(response.data)
         if (response.data.result.userId) {
-          const { userId, jwt } = response.data.result;
-          this.userId = userId;
-          this.accessToken = jwt.accessToken;
-          this.refreshToken = jwt.refreshToken;
+          const { userId, jwt } = response.data.result
+          this.userId = userId
+          this.accessToken = jwt.accessToken
+          this.refreshToken = jwt.refreshToken
 
           // 사용자 정보 가져오기
-          const userInfo = await this.fetchUserInfo(userId);
+          const userInfo = await this.fetchUserInfo(userId)
           if (userInfo) {
-            console.log('User signed in:', this.userInfo);
+            console.log('User signed in:', this.userInfo)
 
             // 팀 정보 가져오기
-            const teamStore = useTeamStore();
+            const teamStore = useTeamStore()
             if (Array.isArray(userInfo.teamList) && userInfo.teamList.length > 0) {
-              await Promise.all(
-                userInfo.teamList.map(teamId => teamStore.fetchTeamById(teamId))
-              );
+              await Promise.all(userInfo.teamList.map((teamId) => teamStore.fetchTeamById(teamId)))
             }
 
             // 로그인 성공 후 HomeView로 리디렉션
-            router.push({ name: 'HomeView' });
+            router.push({ name: 'HomeView' })
 
-            return { success: true, message: response.data.message };
+            return { success: true, message: response.data.message }
           } else {
-            return { success: false, message: 'Failed to fetch user info' };
+            return { success: false, message: 'Failed to fetch user info' }
           }
         } else {
-          errorStore.showError(response.data.message);
-          return { success: false, message: response.data.message };
+          errorStore.showError(response.data.message)
+          return { success: false, message: response.data.message }
         }
       } catch (error) {
-        errorStore.showError('Failed to sign in');
-        return { success: false, message: error.message };
+        errorStore.showError('Failed to sign in')
+        return { success: false, message: error.message }
       }
     },
 
     async signUp({ id, name, email, password, language }) {
-      const errorStore = useErrorStore();
+      const errorStore = useErrorStore()
       try {
         const signupResponse = await axiosInstance.post('api/users', {
           id,
@@ -99,57 +97,57 @@ export const useUserStore = defineStore('user', {
           email,
           password,
           language
-        });
+        })
         console.log(signupResponse)
         if (signupResponse.data.success) {
-          return { success: true, result: signupResponse.data.result };
+          return { success: true, result: signupResponse.data.result }
         } else {
           errorStore.showError(signupResponse.data.message)
-          return { success: false, message: signupResponse.data.message };
+          return { success: false, message: signupResponse.data.message }
         }
       } catch (error) {
-        errorStore.showError('Failed to sign up');
+        errorStore.showError('Failed to sign up')
         console.log('here')
-        return { success: false, message: error.message };
+        return { success: false, message: error.message }
       }
     },
 
     async signOut() {
-      const errorStore = useErrorStore();
+      const errorStore = useErrorStore()
       try {
         const headers = {
           Authorization: `Bearer ${this.accessToken}`
-        };
-        console.log('Request Headers:', headers);
-        console.log('userId' ,this.userId);
-        const response = await axiosInstance.post('api/users/logout', { userId: this.userId });
-  
+        }
+        console.log('Request Headers:', headers)
+        console.log('userId', this.userId)
+        const response = await axiosInstance.post('api/users/logout', { userId: this.userId })
+
         if (response.data.success) {
           // 사용자 정보를 초기화
-          this.userId = 0;
-          this.userInfo = {};
-          this.accessToken = null;
-          this.refreshToken = null;
-          console.log('User signed out successfully');
-          return { success: true, message: response.data.message };
+          this.userId = 0
+          this.userInfo = {}
+          this.accessToken = null
+          this.refreshToken = null
+          console.log('User signed out successfully')
+          return { success: true, message: response.data.message }
         } else {
-          errorStore.showError(response.data.message);
-          return { success: false, message: response.data.message };
+          errorStore.showError(response.data.message)
+          return { success: false, message: response.data.message }
         }
       } catch (error) {
-        errorStore.showError('Failed to sign out');
-        return { success: false, message: error.message };
+        errorStore.showError('Failed to sign out')
+        return { success: false, message: error.message }
       }
     },
 
     async checkIdDuplication(id) {
       try {
-        const response = await axiosInstance.get(`api/users/duplication/${id}`);
-        return response.data.result.available;
+        const response = await axiosInstance.get(`api/users/duplication/${id}`)
+        return response.data.result.available
       } catch (error) {
-        throw new Error('ID already exists.');
+        throw new Error('ID already exists.')
       }
-    },
+    }
   },
   persist: {
     enabled: true,
@@ -161,4 +159,4 @@ export const useUserStore = defineStore('user', {
       }
     ]
   }
-});
+})
