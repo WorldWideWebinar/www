ebad038d56abde7d67cc012dbf54c07b246a915f
이주영 <rsb987@naver.com>
2024-07-31 17:32:33 +0900

    feat : 이미지 볼륨 설정

--- BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/team/dto/TeamDto.java
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/team/dto/TeamDto.java
@@ -1,14 +1,14 @@
 package com.ssafy.globalcc.domain.team.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Getter
+@Setter
 @ToString
+@NoArgsConstructor
 public class TeamDto {
     private Integer ownerId;
     private String teamName;
