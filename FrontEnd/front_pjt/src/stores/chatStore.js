d707b68736593641324b50007370909994fb11dc
권용수 <kwonwd@naver.com>
2024-08-08 14:11:14 +0900

    Feat : chatStore 임시 연결

--- FrontEnd/front_pjt/src/stores/chatStore.js
+++ FrontEnd/front_pjt/src/stores/chatStore.js
@@ -1,10 +1,40 @@
 import { defineStore } from 'pinia'
+import axiosInstance from '@/axios';
 
+// export const useMessageStore = defineStore('message', {
+//   state: () => ({
+//     messages: [],
+//   }),
+//   actions: {
+//     addMessage(message) {
+//       this.messages.push(message);
+//     },
+//     setMessages(newMessages) {
+//       this.messages = newMessages;
+//     },
+//     clearMessages() {
+//       this.messages = [];
+//     }
+//   },
+//   getters: {
+//     getMessagesByTeamId: (state) => (teamId) => {
+//       return state.messages.filter(message => message.team_id === teamId);
+//     }
+//   }
+// });
 export const useMessageStore = defineStore('message', {
   state: () => ({
     messages: [],
   }),
   actions: {
+    async fetchMessagesByTeamId(teamId) {
+      try {
+        const response = await axiosInstance.get(`/api/chats/team/${teamId}`);
+        this.setMessages(response.data);
+      } catch (error) {
+        console.error('Failed to fetch messages:', error);
+      }
+    },
     addMessage(message) {
       this.messages.push(message);
     },
@@ -13,11 +43,6 @@ export const useMessageStore = defineStore('message', {
     },
     clearMessages() {
       this.messages = [];
-    }
-  },
-  getters: {
-    getMessagesByTeamId: (state) => (teamId) => {
-      return state.messages.filter(message => message.team_id === teamId);
     }
   }
 });
