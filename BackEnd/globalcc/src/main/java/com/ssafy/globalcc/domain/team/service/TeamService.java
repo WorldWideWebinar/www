d39ab931552790d71d7bf15b83dd3c790bedf573
JuYoung Lee <rsb987@naver.com>
2024-08-12 12:29:49 +0900

    feat: get Emojis api 생성

--- BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/team/service/TeamService.java
+++ BackEnd/globalcc/src/main/java/com/ssafy/globalcc/domain/team/service/TeamService.java
@@ -4,6 +4,8 @@ import com.ssafy.globalcc.domain.team.dto.TeamDto;
 import com.ssafy.globalcc.domain.team.dto.TeamDetailDto;
 import com.ssafy.globalcc.domain.team.dto.TeamOutDto;
 import com.ssafy.globalcc.domain.team.entity.Team;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 
 public interface TeamService {
     int addTeam(TeamDto dto);
@@ -19,4 +21,6 @@ public interface TeamService {
     Team getTeamByIdAndOwnerUid(int teamId, String username);
 
     void updateTeam(int teamId, TeamDto teamDto, String ownerUid);
+
+    ResponseEntity<?> getEmojis();
 }
